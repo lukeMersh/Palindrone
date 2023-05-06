5548f009600f26ac9aac3b0e79ab5512b962ca29
luke mersh <lukemersh5@gmail.com>
2023-05-06 13:41:30 +0100

    program breakdown

--- index.php
+++ index.php
@@ -1,31 +1,34 @@
+
+
+!--
+  Big Problem- Find a palidrone from a given string
+ --!
+!--Small problem #1 - Get the string as a user input into a variable
+--!
+
+<form method="post">
+    Enter a Number/String: <input type="text" name="string"/><br>
+    <button type="submit">Check</button>
+</form>
+
 <?php
-
-/**
- * Big Problem- Find a palidrone from a given string
- */
-/**
- * Small problem #1 - Get the string as a user input into a variable
- */
-
-
-
-
-
-
+if($_POST)
+{
+    $string = $_POST['string'];
 /**
  * Step 2- Reverse the user input and store it in seperate variable
  */
+    $reverse = strrev($string);
 
-
-
+}
 /**
- * Step 3-compare the variables
+ * Step 3-compare the variables and Based on the comparison results, display the results in a user understandable format
  */
-
-
-
-/**
- * Baes on the comparison results, display the results in a user understandable format
- *
- */
+if($string == $reverse){
+            echo "The number/string $string is Palindrome";
+        }else{
+            echo "The number/string $string is not a Palindrome";
+        }
+}
+      ?>
 
