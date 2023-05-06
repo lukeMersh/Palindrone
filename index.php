b675ac07b6c4354639c57c7d7c7bc10cf37539aa
luke mersh <lukemersh5@gmail.com>
2023-05-06 13:09:28 +0100

    commented steps

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Big Problem- Find a palidrone from a given string
+ */
+/**
+ * Small problem #1 - Get the string as a user input into a variable
+ */
+
+
+
+
+
+
+/**
+ * Step 2- Reverse the user input and store it in seperate variable
+ */
+
+
+
+/**
+ * Step 3-compare the variables
+ */
+
+
+
+/**
+ * Baes on the comparison results, display the results in a user understandable format
+ *
+ */
+
